d534a477488e6ab4a8a0b9467aff3e100cecba04
worasetx8 <146795818+worasetx8@users.noreply.github.com>
2024-12-09 15:00:39 +0700

    add api key3

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(apiKeyMiddleware);
+app.use(apiKeyMiddleware);
 app.use("/order", orderRouter);
 
 app.get("/", (req, res) => res.render("successed.ejs"));
--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,131 +1,8 @@
-// const axios = require("axios");
-// const https = require("https");
-// const moment = require("moment");
-// require("dotenv").config();
-// const isEmpty = require("lodash.isempty");
-// const log = require("../middleware/logger");
-// const dateHelper = require("../helpers/dateHelper");
-// const db = require("../services/connectionService");
-
-// const addOrderModel = async (body, headers) => {
-//   const connection = await db.getConnection();
-//   try {
-//     if (!isEmpty(body)) {
-//       await responseResultOrder(body, headers);
-//       return body.number;
-//     }
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     connection?.release();
-//   }
-// };
-// const updateOrderModel = async (body, headers) => {
-//   const connection = await db.getConnection();
-//   try {
-//     if (!isEmpty(body)) {
-//       await responseResultOrder(body, headers);
-//       return body.number;
-//     }
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     connection?.release();
-//   }
-// };
-// const updatePaymentOrderModel = async (body, headers) => {
-//   const connection = await db.getConnection();
-//   try {
-//     if (!isEmpty(body)) {
-//       await responseResultOrder(body, headers);
-//       return body.number;
-//     }
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     connection?.release();
-//   }
-// };
-// const updateTrackingOrderModel = async (body, headers) => {
-//   const connection = await db.getConnection();
-//   try {
-//     if (!isEmpty(body)) {
-//       await responseResultOrder(body, headers);
-//       // [{"trackingno":"THP0001","trackingurl":"","shippingdate":null}]
-//       const result = body.map((item) => item.trackingno);
-//       return result;
-//     }
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     connection?.release();
-//   }
-// };
-// const deleteOrderModel = async (body, headers) => {
-//   const connection = await db.getConnection();
-//   try {
-//     if (!isEmpty(body)) {
-//       await responseResultOrder(body, headers);
-//       return body.number;
-//     }
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     connection?.release();
-//   }
-// };
-
-// const getUrl = async () => {
-//   const connection = await db.getConnection();
-//   try {
-//     const query = "SELECT url FROM web_links WHERE active = 1";
-//     const [data] = await connection.query(query);
-//     return data.length > 0 ? { result: data } : null;
-//   } catch (error) {
-//     throw error;
-//   } finally {
-//     connection?.release();
-//   }
-// };
-
-// const responseResultOrder = async (body, headers) => {
-//   const url = await getUrl();
-//   console.log("url", url);
-
-//   if (!isEmpty(url)) {
-//     for (const link of url?.result) {
-//       return axios({
-//         method: "post",
-//         url: `${link.url}`,
-//         headers: { headers },
-//         data: { body },
-//         httpsAgent: new https.Agent({ rejectUnauthorized: false })
-//       })
-//         .then((response) => {
-//           return response;
-//         })
-//         .catch((error) => {
-//           console.log(error.response.data.message);
-//           throw new Error(error.response.data.message);
-//         });
-//     }
-//   }
-// };
-
-// module.exports = {
-//   addOrderModel,
-//   updateOrderModel,
-//   updatePaymentOrderModel,
-//   updateTrackingOrderModel,
-//   deleteOrderModel
-// };
-
 const axios = require("axios");
 const https = require("https");
 const isEmpty = require("lodash.isempty");
 const db = require("../services/connectionService");
 
-// Helper function to handle database connections
 const withDbConnection = async (callback) => {
   const connection = await db.getConnection();
   try {
@@ -137,7 +14,6 @@ const withDbConnection = async (callback) => {
   }
 };
 
-// Base model for order operations
 const orderModelBase = async (body, headers, processCallback) => {
   if (isEmpty(body)) return null;
 
@@ -150,14 +26,12 @@ const orderModelBase = async (body, headers, processCallback) => {
   return resultData;
 };
 
-// Generic order models
 const addOrderModel = (body, headers) => orderModelBase(body, headers);
 const updateOrderModel = (body, headers) => orderModelBase(body, headers);
 const updatePaymentOrderModel = (body, headers) => orderModelBase(body, headers);
 const updateTrackingOrderModel = (body, headers) => orderModelBase(body, headers, (body) => body.map((item) => item.trackingno));
 const deleteOrderModel = (body, headers) => orderModelBase(body, headers);
 
-// Fetch active URLs from the database
 const getUrl = () =>
   withDbConnection(async (connection) => {
     const query = "SELECT url FROM web_links WHERE active = 1";
@@ -165,7 +39,6 @@ const getUrl = () =>
     return data.length > 0 ? { result: data } : null;
   });
 
-// Send data to external URLs
 const responseResultOrder = async (body, headers) => {
   const urlData = await getUrl();
 
@@ -176,16 +49,14 @@ const responseResultOrder = async (body, headers) => {
   for (const { url } of urls) {
     try {
       const trimmedUrl = url.trim();
-      // const response = await axios({
-      //   method: "post",
-      //   url,
-      //   headers,
-      //   data: body,
-      //   httpsAgent: new https.Agent({ rejectUnauthorized: false })
-      // });
-      console.log("method:", "post\n", "url:", `${trimmedUrl}\n`, "headers = ", headers, "data = ", body);
-      const response = trimmedUrl;
-      responses.push(response);
+      await axios.post(trimmedUrl, {
+        headers: headers,
+        data: body
+        // httpsAgent: new https.Agent({ rejectUnauthorized: false })
+      });
+
+      const responsePush = trimmedUrl;
+      responses.push(responsePush);
     } catch (error) {
       console.error("Error:", error.response?.data?.message || error.message);
       throw error;
